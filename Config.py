314d91e3fad0b7d6c0755d2f6ed94f9c304908ce
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2022-07-29 01:03:24 +0700

    Initial commit

---
+++ Config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config(object):
+    API_ID = int(os.environ.get("APP_ID", "9743709"))
+    API_HASH = os.environ.get("API_HASH", "b26f8b044ebbc7f6a474fc7e2e962d4a")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5109381626:AAH0cosvoJrmSOBx1I2E1i3aPuOojkGd-xI")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
+    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "@MircMusicbot")
+    SUPPORT = os.environ.get("SUPPORT", "MCMusicz")
+    CHANNEL = os.environ.get("CHANNEL", "MCMusicz")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/5b39746a9cbb3b7273c3a.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
+    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "2"))
---
+++ Zaid/plugins/start.py
@@ -0,0 +1,44 @@
+from Zaid import Zaid, BOT_USERNAME
+from Config import Config
+from telethon import events, Button
+
+PM_START_TEXT = """
+ʜᴇʏᴀ! {}
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+Halo ! , Saya Adalah Mirc Music Bot. Siap Menemani Anda Memutar Music Dengan Mudah Di Voice Chat Groups.!
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+✘ **ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ 🔘 ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ℹ️**.
+"""
+
+@Zaid.on(events.NewMessage(pattern="^[?!/]start$"))
+async def start(event):
+    if Config.MANAGEMENT_MODE == "ENABLE":
+        return
+    if event.is_private:
+       await event.client.send_file(event.chat_id,
+             Config.START_IMG,
+             caption=PM_START_TEXT.format(event.sender.first_name), 
+             buttons=[
+        [Button.url("➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀᴛ", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+        [Button.url("👨‍ Owner", "https://t.me/https://t.me/@riasyee")],
+        [Button.url("🗣️ ꜱᴜᴘᴘᴏʀᴛ", f"https://t.me/{Config.SUPPORT}"), Button.url("📣 ᴜᴘᴅᴀᴛᴇꜱ", f"https://t.me/{Config.CHANNEL}")],
+        [Button.inline("ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅꜱ", data="help")]])
+       return
+
+    if event.is_group:
+       await event.reply("**Halo. Saya Aktif ✅**")
+       return
+
+
+
+@Zaid.on(events.callbackquery.CallbackQuery(data="start"))
+async def _(event):
+    if Config.MANAGEMENT_MODE == "ENABLE":
+        return
+    if event.is_private:
+       await event.edit(PM_START_TEXT.format(event.sender.first_name), buttons=[
+        [Button.url("➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀᴛ", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+        [Button.url("👨‍💻 Owner", "https://t.me/https://t.me/@riasyee")],
+        [Button.url("🗣️ ꜱᴜᴘᴘᴏʀᴛ", f"https://t.me/{Config.SUPPORT}"), Button.url("📣 ᴜᴘᴅᴀᴛᴇꜱ", f"https://t.me/{Config.CHANNEL}")],
+        [Button.inline("ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅꜱ", data="help")]])
+       return
